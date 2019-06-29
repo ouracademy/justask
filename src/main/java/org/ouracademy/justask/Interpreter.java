89b3525f200cacab1b4feb4294920b9ef0a842fd
artmadeit <this.arthur.email@gmail.com>
2019-06-29 17:57:32 -0500

    feat: use command list

--- src/main/java/org/ouracademy/justask/Interpreter.java
+++ src/main/java/org/ouracademy/justask/Interpreter.java
@@ -3,35 +3,35 @@ package org.ouracademy.justask;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.List;
 
 /**
  * Interpreter
  */
 public class Interpreter {
     Robot robot;
+    List<String> commands = List.of("type");
 
     public Interpreter(Robot robot) {
         this.robot = robot;
     }
 
     public void interpret(String text) {
-        var command = text.split(" ")[0];
-        var args = text.substring(command.length() + 1);
+        var command = this.commands.stream().filter(x -> text.startsWith(x)).findFirst();
+        var args = text.substring(command.orElseThrow().length() + 1);
 
         System.out.println("'" + args + "'");
         for (int charUnicodeIndex : args.chars().toArray()) {
-            if (command.equals("type")) {
-                if ('.' == (char) charUnicodeIndex) {
-                    type(robot, KeyEvent.VK_PERIOD);
-                } else if (' ' == (char) charUnicodeIndex) {
-                    type(robot, KeyEvent.VK_SPACE);
-                } else {
-                    int unicodeA = 97;
-                    type(robot, charUnicodeIndex - unicodeA + KeyEvent.VK_A);
-                }
+            if ('.' == (char) charUnicodeIndex) {
+                type(robot, KeyEvent.VK_PERIOD);
+            } else if (' ' == (char) charUnicodeIndex) {
+                type(robot, KeyEvent.VK_SPACE);
+            } else {
+                int unicodeA = 97;
+                type(robot, charUnicodeIndex - unicodeA + KeyEvent.VK_A);
             }
         }
+
     }
 
     private void type(Robot robot, int... keys) {
@@ -39,5 +39,4 @@ public class Interpreter {
         Arrays.stream(keys).forEach(i -> robot.keyPress(i));
         Arrays.stream(keys).forEach(i -> robot.keyRelease(i));
     }
-
 }
--- src/test/java/org/ouracademy/justask/InterpreterTests.java
+++ src/test/java/org/ouracademy/justask/InterpreterTests.java
@@ -6,12 +6,15 @@ import static org.mockito.Mockito.verify;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
@@ -33,5 +36,12 @@ public class InterpreterTests {
         return Stream.of(arguments("hi", List.of(KeyEvent.VK_H, KeyEvent.VK_I)),
                 arguments("c.", List.of(KeyEvent.VK_C, KeyEvent.VK_PERIOD)),
                 arguments("a b", List.of(KeyEvent.VK_A, KeyEvent.VK_SPACE, KeyEvent.VK_B)));
+    }
+
+    @Test
+    public void interpretNoCommand() {
+        var robot = mock(Robot.class);
+        var interpreter = new Interpreter(robot);
+        assertThrows(NoSuchElementException.class, () -> interpreter.interpret("this is not a command :("));
     }
 }
