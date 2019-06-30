86e5c9b5d6d5faa1b941587c31ddb1c97ee31246
artmadeit <this.arthur.email@gmail.com>
2019-06-29 23:44:11 -0500

    feat: add number keymappings

--- src/main/java/org/ouracademy/justask/TypeCommand.java
+++ src/main/java/org/ouracademy/justask/TypeCommand.java
@@ -5,6 +5,7 @@ import static java.util.Map.entry;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -19,10 +20,15 @@ public class TypeCommand extends Command {
     public Map<Character, Integer> charToKeyboardMap;
 
     public TypeCommand() {
+
         this.charToKeyboardMap = Stream
-                .concat(Stream.of(entry('.', KeyEvent.VK_PERIOD), entry(' ', KeyEvent.VK_SPACE)),
-                        IntStream.rangeClosed('a', 'z').mapToObj(x -> entry((char) x, x - 'a' + KeyEvent.VK_A)))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                .of(Stream.of(entry('.', KeyEvent.VK_PERIOD), entry(' ', KeyEvent.VK_SPACE)),
+                        keyboardMappingFor('a', 'z', KeyEvent.VK_A), keyboardMappingFor('0', '9', KeyEvent.VK_0))
+                .flatMap(i -> i).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private Stream<Entry<Character, Integer>> keyboardMappingFor(char start, char end, int startKey) {
+        return IntStream.rangeClosed(start, end).mapToObj(x -> entry((char) x, x - start + startKey));
     }
 
     public String getName() {
